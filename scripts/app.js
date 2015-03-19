2cde8f2d7d57e89c83a3e465ffeeee58895ac734
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-03-19 09:33:08 +0100

    refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -9,43 +9,10 @@
 //5 app controller
 //6 directive link
 
-(function () { 
-var app = angular.module('coluApp', ['openlayers-directive']);
+
+ angular.module('coluApp', ['openlayers-directive']);
 
 
-var ships = [
-            {
-                name: 'Jonken1',
-                lat: 51.505,
-                lon: -2.09
-            },
-            {
-                name: 'Jonken2',
-                lat: 51.375,
-                lon: -20.35
-            },
-            {
-                name: 'Jonken3',
-                lat: 51.267,
-                lon: -10.083
-            }
-        ];
 
- app.controller('mainController', [ '$scope', function($scope) {
- 
-             angular.extend($scope, {
-                 center: {
-                     lat: ships[0].lat,
-                     lon: ships[1].lon,
-                     zoom: 4
-                 },
-
-              	ships: ships
-
-             });
-             
-  }]);
-
-})();
 
 
--- scripts/main.controller.js
+++ scripts/main.controller.js
@@ -1,7 +1,34 @@
 
+angular.module('coluApp')
+.controller('mainController', function($scope){
+  	
+      	$scope.ships = [
+            {
+                name: 'Jonken1',
+                lat: 51.505,
+                lon: -2.09
+            },
+            {
+                name: 'Jonken2',
+                lat: 51.375,
+                lon: -20.35
+            },
+            {
+                name: 'Jonken3',
+                lat: 51.267,
+                lon: -10.083
+            }
+        ];
+        
+		$scope.center = {
+             lat: $scope.ships[0].lat,
+             lon: $scope.ships[1].lon,
+             zoom: 4
+         }
 
-app.module('coluApp')
-  .controller('MainController', function($scope){
 
+             
+             
+ 
     
 });
