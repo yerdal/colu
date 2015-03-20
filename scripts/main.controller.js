e3ad72d06579a00c3b4a77164553992bf0537342
oankarberg <oankarberg@gmail.com>
2015-03-20 09:24:51 +0100

    merged GRIB functions from gribbranch

--- scripts/main.controller.js
+++ scripts/main.controller.js
@@ -4,7 +4,10 @@ angular.module('coluApp')
 .controller('mainController', function($scope, $http){
     
 
-    $scope.ship = {name: 'JOnk', lat: 51.04, lon: -20.32};
+    
+
+
+
 
     var map = new ol.Map({
       layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
@@ -14,12 +17,44 @@ angular.module('coluApp')
         zoom: 2
       })
     });
-        
 
+    var initGribData = function(){
+      var gribSource = new ol.source.TileWMS({
+          url: 'http://localhost:8080/geoserver/wms',
+                    params: {'LAYERS': 'cite:Temperature_surface'},
+                    serverType: 'geoserver'
+                    //crossOrigin: ''
+                    
+      });
+      var gribLayer = new ol.layer.Tile({
+        source: gribSource,
+        opacity: 0.5
+      });
+      var view = new ol.View({
+        center: [0, 0],
+        zoom: 1
+      });
+       
+      map.addLayer(gribLayer);
+
+      map.on('pointermove', function(evt) {
+        document.getElementById('info').innerHTML = '';
+        var viewResolution = /** @type {number} */ (view.getResolution());
+        var url = gribSource.getGetFeatureInfoUrl(
+          evt.coordinate, viewResolution, 'EPSG:3857',
+          {'INFO_FORMAT': 'text/html'});
+        if (url) {
+          document.getElementById('info').innerHTML =
+            '<iframe seamless src="' + url + '"></iframe>';
+        }
+      });
+    }
+    initGribData();
          //Get data from backend
-    $http.get('http://localhost:8080/ships/test').success(function(data,status,headers,config)
+    $http.get('http://localhost:8090/ships/test').success(function(data,status,headers,config)
     {
       initShipPos(data);
+
       //drawLines(data);
 
     }).error(function(data,status,headers,config){
