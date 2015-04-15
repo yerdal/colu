53a50f5aa81dc6e87333114f006f84f3cc55148a
annageorgellis <annageorgelli@hotmail.com>
2015-04-15 11:17:12 +0200

    extended form

--- scripts/status-app.js
+++ scripts/status-app.js
@@ -5,8 +5,16 @@ angular.module('coluApp', [])
 
     $scope.fuel = {upperLimit: '250'};
 
+    $scope.combinedWave = {upperLimit: '250'};
 
-    $scope.editorEnabled = [false, false];
+    $scope.current = {upperLimit: '250'};
+
+    $scope.wind = {upperLimit: '250'};
+
+    $scope.velocity = {upperLimit: '250'};
+
+
+    $scope.editorEnabled = [false, false, false, false, false, false];
     
     $scope.enableEditor = function(id) {
       $scope.editorEnabled[id] = true;
