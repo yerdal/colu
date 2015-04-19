05a32048474b22b69f1f37b6adff2d763cc6ea12
johanhenrikssn <johan.c.henriksson@gmail.com>
2015-04-19 23:19:19 +0200

    minor cleanup

--- scripts/main.controller2.js
+++ scripts/main.controller2.js
@@ -29,34 +29,32 @@ coluApp.controller('mainController', function($scope, $http, sharedProperties ){
     $scope.activeShip = sharedProperties.getActive();
   }
 
-
   $scope.time = {lowerLimit: '-30',
                    upperLimit: '30'};
 
-    $scope.fuel = {upperLimit: '250'};
+  $scope.fuel = {upperLimit: '250'};
 
-    $scope.combinedWave = {upperLimit: '250'};
+  $scope.combinedWave = {upperLimit: '250'};
 
-    $scope.current = {upperLimit: '250'};
+  $scope.current = {upperLimit: '250'};
 
-    $scope.wind = {upperLimit: '250'};
+  $scope.wind = {upperLimit: '250'};
 
-    $scope.velocity = {upperLimit: '250'};
+  $scope.velocity = {upperLimit: '250'};
 
 
-    $scope.editorEnabled = [false, false, false, false, false, false];
-    
-    $scope.enableEditor = function(id) {
-      $scope.editorEnabled[id] = true;
-    };
-    
-    $scope.disableEditor = function(id) {
-      $scope.editorEnabled[id] = false;
-    };
-    
-    $scope.save = function(id) {
-      $scope.disableEditor(id);
-    };
-      
+  $scope.editorEnabled = [false, false, false, false, false, false];
+  
+  $scope.enableEditor = function(id) {
+    $scope.editorEnabled[id] = true;
+  };
+  
+  $scope.disableEditor = function(id) {
+    $scope.editorEnabled[id] = false;
+  };
+  
+  $scope.save = function(id) {
+    $scope.disableEditor(id);
+  };
 
 });
