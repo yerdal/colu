f69f79686777f432a123a9b16223ec48f38472ed
annageorgellis <annageorgelli@hotmail.com>
2015-04-20 10:32:05 +0200

    the form is saved for every ship object

--- scripts/main.controller2.js
+++ scripts/main.controller2.js
@@ -8,47 +8,50 @@ angular.module('coluApp')
       
       console.log("skepp", data[0]);
       $scope.ships = data.slice(1, 10);
+
+      for(var i = 0; i < $scope.ships.length; i++)
+      {
+
+        $scope.ships[i].time = {lowerLimit: '-30',
+                      upperLimit: '30'};
+
+        $scope.ships[i].fuel = {upperLimit: '250'};
+
+        $scope.ships[i].combinedWave = {upperLimit: '250'};
+
+        $scope.ships[i].current = {upperLimit: '250'};
+
+        $scope.ships[i].wind = {upperLimit: '250'};
+
+        $scope.ships[i].velocity = {upperLimit: '250'};   
+        
+      }    
       
       }).error(function(data,status,headers,config){
         console.log('ERROR getting from backend' , status);
+
       });
-  
-
-  $scope.activeShip = "Default";
- 
-
- $scope.showActive = function(s){
-
-    $scope.activeShip = s;  
-  }
-
-  $scope.time = {lowerLimit: '-30',
-                   upperLimit: '30'};
-
-    $scope.fuel = {upperLimit: '250'};
-
-    $scope.combinedWave = {upperLimit: '250'};
-
-    $scope.current = {upperLimit: '250'};
-
-    $scope.wind = {upperLimit: '250'};
-
-    $scope.velocity = {upperLimit: '250'};
-
 
     $scope.editorEnabled = [false, false, false, false, false, false];
-    
+
     $scope.enableEditor = function(id) {
-      $scope.editorEnabled[id] = true;
+        $scope.editorEnabled[id] = true;
     };
-    
+        
     $scope.disableEditor = function(id) {
       $scope.editorEnabled[id] = false;
     };
-    
+        
     $scope.save = function(id) {
       $scope.disableEditor(id);
     };
-      
+  
+    $scope.activeShip = "Default";
+
+    $scope.showActive = function(s){
+
+    $scope.activeShip = s;  
+
+  }
 
 });
