fe306055bcbd4a7546df5bdf81ac85d2546e5e1b
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-04-22 14:29:13 +0200

    now is work

--- Rest/src/main/java/ship/ShipController.java
+++ Rest/src/main/java/ship/ShipController.java
@@ -93,100 +93,7 @@ public class ShipController extends ParsingXML{
       return voyageList;
     }
 
-<<<<<<< HEAD
-    private ArrayList getXMLShip(){
-        ArrayList<Ship> shipsArray = new ArrayList<Ship>();
-        try {
-     
-          // OSKAR C:/Users/Oskar Ankarberg/Desktop/Voyage_and_shipdata
-          // Mattias /Users/mattiaspalmgren/Dropbox/MT/temp/Voyage_and_ship_data/ships.xml
-          // Johan 
-          File fXmlFile = new File("../data/voyage_and_ship/ships.xml");
-
-          DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-          Document doc = dBuilder.parse(fXmlFile);
-        
-          //optional, but recommended
-          //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-          doc.getDocumentElement().normalize();
-          
-         
-          NodeList nList = doc.getElementsByTagName("ship");
-            //System.out.println(nList.getLength() + "\n" + "\n"); 
-            for (int temp = 1; temp < nList.getLength(); temp++) 
-            {
-                Node nNode = nList.item(temp); //get the second Ship node
-  
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) 
-                {
-                    Element eElement = (Element) nNode;
-                    int shipID = Integer.parseInt(eElement.getAttribute("id"));
-
-                    String shipValues = eElement.getAttribute("values");
-                    
-                    String[] shipParts = shipValues.split(";", 34);
-                  
-                    
-                    String shipComment = eElement.getElementsByTagName("comment").item(0).getTextContent();
-                    String shipDesc = eElement.getElementsByTagName("description").item(0).getTextContent();
-          
-                    Operator operator =  new Operator(Integer.parseInt(shipParts[0]));
-
-                    Ship theShip = new Ship(shipID, operator, shipParts[1], shipParts[2], shipParts[3], shipParts[4], shipParts[5], shipParts[6], shipParts[7], shipParts[8], shipParts[9], shipParts[10],
-                        shipParts[11], shipParts[12], shipParts[13], shipParts[14], shipParts[15], shipParts[16], shipParts[17], shipParts[18], shipParts[19], shipParts[20], shipParts[21], shipParts[22], 
-                        shipParts[23], shipParts[24], shipParts[25], shipParts[26], shipParts[27], shipParts[28], shipParts[29], shipParts[30], shipParts[31], shipParts[32], shipParts[33], shipComment, shipDesc);
-                    shipsArray.add(theShip);
-                    
-                }
-                NodeList contactList = doc.getElementsByTagName("contact");
-                for (int k = 1; k < contactList.getLength(); k++)
-                {
-                    Node contactNode = contactList.item(k);
-
-                    if (contactNode.getNodeType() == Node.ELEMENT_NODE)
-                    {
-                      Element eElement = (Element) contactNode;
-                      int contactID  = Integer.parseInt(eElement.getAttribute("id"));
-                      String contactValues = eElement.getAttribute("values");
-                      String[] contactParts = contactValues.split(";", 4);
-                      System.out.println(" \n Contact VALUES  ");
-                      for (int i = 0; i < contactParts.length; i++)
-                      {
-                        System.out.println(contactParts[i] + " ");
-                      }
-
-                    }
-                }
-                NodeList operatorList = doc.getElementsByTagName("operator");
-                for (int j = 1; j < operatorList.getLength(); j++)
-                {
-                    Node operatorNode = operatorList.item(j);
-                    if (operatorNode.getNodeType() == Node.ELEMENT_NODE)
-                    {
-                      Element eElement = (Element) operatorNode;
-                      int operatorID = Integer.parseInt(eElement.getAttribute("id"));
-                      String operatorValues = eElement.getAttribute("values");
-                      String [] operatorParts = operatorValues.split(";", 5);
-                      System.out.println(" \n OPERATOR VALUE  ");
-                      for (int n = 0; n < operatorParts.length; n++)
-                      {
-                        System.out.println(operatorParts[n] + " ");
-                      }
-                    }
-                }
-
-                
-
-
-
-          }
-          return shipsArray;
-
-        
-        } catch (Exception e) {
-             e.printStackTrace();
-=======
+    
     @RequestMapping(value="/voyages/status/bad")
     public ArrayList getBadVoyages() {
       //so we know we have the ongoing voyages
@@ -216,98 +123,12 @@ public class ShipController extends ParsingXML{
         if (tempVoyage.checkStatus() == "GOOD")
         {
           goodVoyages.add(tempVoyage);
->>>>>>> master
         }
       } 
       return goodVoyages;
     }
 
-<<<<<<< HEAD
-    private ArrayList getXMLShipPolls(){
-        ArrayList<Ship> shipsArray = new ArrayList<Ship>();
-        
-        try {
-          // Mattias: /Users/mattiaspalmgren/Dropbox/MT/temp/Voyage_and_ship_data/polls.xml
-          // OSKAR C:/Users/Oskar Ankarberg/Desktop/Voyage_and_shipdata
-          // 
-          File fXmlFile = new File("../data/voyage_and_ship/polls.xml");
 
-          DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-          Document doc = dBuilder.parse(fXmlFile);
-        
-          //optional, but recommended
-          //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-          doc.getDocumentElement().normalize();
-          
-         
-          NodeList nList = doc.getElementsByTagName("ship");
-            //System.out.println(nList.getLength() + "\n" + "\n"); 
-            for (int temp = 1; temp < nList.getLength(); temp++) 
-            {
-                Node nNode = nList.item(temp); //get the second Ship node
-    
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) 
-                {
-                  //New array with positions for every ship 
-                  ArrayList<SatCPollPosition> satCPollArray = new ArrayList<SatCPollPosition>();
-
-                  Element tempShipElement = (Element) nNode;
-                  int shipID = Integer.parseInt(tempShipElement.getAttribute("id"));
-                  String shipValues = tempShipElement.getAttribute("values");
-
-                  String[] shipParts = shipValues.split(";", 34);
-                  //get the operator ID  shipParts[0]
-                  Operator tempOperator = new Operator(Integer.parseInt(shipParts[0])); 
-                  Ship tempShip = new Ship(shipID, tempOperator, shipParts[4]);
-                  
-                  NodeList nList_pos = tempShipElement.getElementsByTagName("satcpollposition");
-
-                  //System.out.println(nList.getLength() + "\n" + "\n"); 
-                  for (int j = 0; j < nList_pos.getLength(); j++) 
-                  {
-                    Node positionNode = nList_pos.item(j);
-
-                    if (positionNode.getNodeType() == Node.ELEMENT_NODE)
-                    {
-                      Element eSatCPosElement = (Element) positionNode;
-                      double lon  = Double.parseDouble(eSatCPosElement.getAttribute("lon"));
-                      double lat  = Double.parseDouble(eSatCPosElement.getAttribute("lat"));
-                      int id  = Integer.parseInt(eSatCPosElement.getAttribute("id"));
-                      String date = eSatCPosElement.getAttribute("date");
-                      String posValues = eSatCPosElement.getAttribute("values");
-                      String[] posParts = posValues.split(";", 5);
-                      //  System.out.println(" \n Posiiton VALUES  ");
-                      // for (int i = 0; i < posParts.length; i++)
-                      // {
-                      //   System.out.println(posParts[i] + " ");
-                      // }
-
-                      SatCPollPosition position = new SatCPollPosition(id, lon, lat, date, 
-                           parseDoubleSafely(posParts[0]), parseDoubleSafely(posParts[1]), posParts[2], parseDoubleSafely(posParts[3]));
-                      
-                      satCPollArray.add(position);
-                    }
-
-
-                  }
-                   
-                  tempShip.setPollPositions(satCPollArray); 
-                  shipsArray.add(tempShip);
-                    
-                }
-
-                              
-
-
-
-          }
-          return shipsArray;
-
-        
-        } catch (Exception e) {
-             e.printStackTrace();
-=======
     @RequestMapping(value="/voyages/status/ok")
     public ArrayList getOkVoyages()
     {
@@ -321,7 +142,6 @@ public class ShipController extends ParsingXML{
         if (tempVoyage.checkStatus() == "OK")
         {
           okVoyages.add(tempVoyage);
->>>>>>> master
         }
       } 
       return okVoyages;
