878fa6cf618f59ce81ebfaf424fe92a2ddb2ee8c
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-04-22 15:24:58 +0200

    loads voyages on frontend

--- scripts/main.controller2.js
+++ scripts/main.controller2.js
@@ -2,71 +2,75 @@ var coluApp = angular.module('coluApp');
 
 coluApp.service('sharedProperties', function() {
     
-    var activeShip = "Default";
+    var activeVoyage = "Default";
     return {
         getActive: function() {
-            return activeShip;
+            return activeVoyage;
         },
         setActive: function(s) {
-            activeShip = s;
+            activeVoyage = s;
         }
     }
 });
 
 coluApp.controller('mainController', function($scope, $http, sharedProperties ){
 
-  //Gets the ship-data
-  $http.get('http://localhost:8090/ships/test').success(function(data,status,headers,config)
+  //Gets the Voyage-data
+  $http.get('http://localhost:8090/voyages').success(function(data,status,headers,config)
     {
       
-      //For now, just looking at a few ships
-      $scope.ships = data.slice(1, 20);
+      //For now, just looking at a few voyages
+      $scope.voyages = data.slice(1, 5);
+      console.log("fesfsdfsdfs", data[0]);
 
       //Sets some hardcoded parameters
-      for(var i = 0; i < $scope.ships.length; i++)
+      for(var i = 0; i < $scope.voyages.length; i++)
       {
-        $scope.ships[i].rangeParameters = {
+        $scope.voyages[i].rangeParameters = {
           time: {label: "Tid", lowerLimit: '-30', upperLimit: '30', status: true, unit: "minuter", number: 0},
           velocity: {label: "Hastighet", lowerLimit: '50', upperLimit: '250', status: false, unit: "knop", number: 1}
         }
+
+        //console.log("oefsn", $scope.voyages[8]);
         
-        // $scope.ships[i].warning = {warning: ': '
+        // $scope.voyages[i].warning = {warning: ': '
         //                           };
                                   
-        // if($scope.ships[i].time.status == 'false')
+        // if($scope.voyages[i].time.status == 'false')
         // {
-        //   $scope.ships[i].warning.warning += 'T ';
+        //   $scope.voyages[i].warning.warning += 'T ';
         // } 
 
-        // if($scope.ships[i].fuel.status == 'false')
+        // if($scope.voyages[i].fuel.status == 'false')
         // {
-        //   $scope.ships[i].warning.warning += 'B ';
+        //   $scope.voyages[i].warning.warning += 'B ';
         // } 
 
-        // if($scope.ships[i].combinedWave.status == 'false' || $scope.ships[i].current.status == 'false' || $scope.ships[i].wind.status == 'false')
+        // if($scope.voyages[i].combinedWave.status == 'false' || $scope.voyages[i].current.status == 'false' || $scope.voyages[i].wind.status == 'false')
         // {
-        //   $scope.ships[i].warning.warning += 'V ';
+        //   $scope.voyages[i].warning.warning += 'V ';
         // } 
 
-        // if($scope.ships[i].velocity.status == 'false')
+        // if($scope.voyages[i].velocity.status == 'false')
         // {
-        //   $scope.ships[i].warning.warning += 'F ';
+        //   $scope.voyages[i].warning.warning += 'F ';
         // }                            
-        $scope.ships[i].singleParameters = {
+        $scope.voyages[i].singleParameters = {
 
           fuel: {label: "Bränsle", upperLimit: '250', status: true, unit: "L/mil", number: 2},
-          combinedWave : {label: "Våghöjd",upperLimit: '250', status:false, unit: "m",number: 3},
+          combinedWave : {label: "Våghöjd", upperLimit: '250', status:false, unit: "m",number: 3},
           current : {label: "Ström", upperLimit: '250', status:true, unit: "m/s", number: 4},
-          wind : {label: "Vind", upperLimit: '250', status:false, unit: "m/s", number: 5}
+          wind : {label: "Vind", upperLimit: $scope.voyages[i].weatherWaypoints[1].windSpeed, status:false, unit: "m/s", number: 5}
+        
         }        
       
       } 
 
       
       
-      $scope.shipsBad = $scope.ships.slice(1, 7);
-      $scope.shipsGood = $scope.ships.slice(7, 9);
-      $scope.shipsHandled = $scope.ships.slice(9, 15);
+      $scope.voyagesBad = $scope.voyages.slice(1, 7);
+      $scope.voyagesGood = $scope.voyages.slice(7, 9);
+      $scope.voyagesHandled = $scope.voyages.slice(9, 15);
       
       //Where all the functionality is
       main();   
@@ -83,27 +87,27 @@ coluApp.controller('mainController', function($scope, $http, sharedProperties ){
     //How the form works
     formFunctionality();
   
-    //Shows the active ship in the detailed view 
+    //Shows the active Voyage in the detailed view 
     $scope.showActive = function(s){
       sharedProperties.setActive(s);
-      $scope.activeShip = sharedProperties.getActive();
+      $scope.activeVoyage = sharedProperties.getActive();
     }
 
-    //Used to only show handle-button on Bad-ships
+    //Used to only show handle-button on Bad-voyages
     $scope.isBad = function(s){
-      return ($scope.shipsBad.indexOf(s) != -1)
+      return ($scope.voyagesBad.indexOf(s) != -1)
     }
 
-    //To "handle" ships, and then put them in the handled-list
+    //To "handle" voyages, and then put them in the handled-list
     $scope.handel = function(s){
-      if(!isInArray(s,$scope.shipsHandled))
-        $scope.shipsHandled.push(s);
+      if(!isInArray(s,$scope.voyagesHandled))
+        $scope.voyagesHandled.push(s);
 
-      var index = $scope.shipsBad.indexOf(s);
+      var index = $scope.voyagesBad.indexOf(s);
       console.log("index", index);
 
       if (index > -1) {
-        $scope.shipsBad.splice(index, 1);
+        $scope.voyagesBad.splice(index, 1);
       } 
     }
 
