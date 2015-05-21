aef0018d3e5c5ca9551616d330b13070c0798921
johanhenrikssn <johan.c.henriksson@gmail.com>
2015-05-21 11:50:47 +0200

    prettier looks

--- scripts/directives/map/map.directive.js
+++ scripts/directives/map/map.directive.js
@@ -24,12 +24,19 @@ coluApp.directive('map', function() {
     templateUrl: 'scripts/directives/map/map.html',
     link: function(scope, element){
       var map = new ol.Map({
-        layers: [new ol.layer.Tile({ preload: 4, source: new ol.source.OSM()})],
-        target: document.getElementById('map'),
-        view: new ol.View({
-          zoom: 6.000,
-          minZoom: 2
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.XYZ({
+              url: 'http://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoiam9oYW5oZW5yaWtzc29uIiwiYSI6IjZkZmZkNDQ2N2EzYmRhYjBhMzUzYjNiNzgwMWRhOWYwIn0.mI8Pclywhn9DD3t9_YaADg'
+            })
           })
+        ],
+        target: 'map',
+        view: new ol.View({
+          center: [0, 0],
+          zoom: 6,
+          minZoom: 2
+        })
       });
 
       var shipVectorSource = new ol.source.Vector({});
